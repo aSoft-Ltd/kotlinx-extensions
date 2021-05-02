83640c66c82ce7c37ae36057436a413bbee023a7
andylamax <andylamax@programmer.net>
2021-05-02 12:34:23 +0300

    Bumped version to 0.0.30

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,21 +1,21 @@
 object vers {
-    val kotlin = "1.4.32"
+    val kotlin = "1.5.0"
     val agp = "4.1.3"
     val nexus_staging = "0.22.0"
 
     object asoft {
-        val builders = "1.3.24"
-        val kotlinx_extensions = "0.0.21"
+        val builders = "1.3.30"
+        val kotlinx_extensions = "0.0.30"
     }
 
     object kotlinx {
-        val html = "0.7.2"
+        val html = "0.7.3"
         val coroutines = "1.4.3"
-        val serialization = "1.1.0"
+        val serialization = "1.2.0"
     }
 
     object wrappers {
-        val extensions = "1.0.1-pre.129-kotlin-1.4.10"
-        val css = "1.0.0-pre.129-kotlin-1.4.10"
+        val extensions = "1.0.1-pre.154-kotlin-1.5.0"
+        val css = "1.0.0-pre.154-kotlin-1.5.0"
     }
 }
